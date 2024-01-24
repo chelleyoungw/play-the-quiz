948a9dea825ec31d0d30f02bce9ee4aa39658a88
Chelle Wood <chelleyoungw@gmail.com>
2024-01-23 23:32:25 -0600

    added interval timer onto click event listener that will count down and send alert at 0 seconds

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,30 @@
+var timerHeader = document.getElementById("time")
 var questionContent = document.getElementById("main");
+var timeLeft = 5;
+
+function startTimer() {
+    var timerInterval = setInterval(function(){
+        timeLeft--;
+        timerHeader.textContent = "Time: " + timeLeft;
+        
+        if(timeLeft === 0) {
+        clearInterval(timerInterval);
+        alert("GAME OVER");
+    }
+    },1000);
+
+}
 
 function startQuiz() {
     var startButton = document.createElement("button");
     startButton.textContent = "START";
     document.body.appendChild(startButton);
-    questionContent.textContent = "Click the start button to begin your quiz.";
     
+    questionContent.textContent = "Click the start button to begin your quiz.";
+
+    startButton.addEventListener('click', function() {
+        startTimer();
+    });
 }
 
 startQuiz();
